d157285d1cac47d820d469dd18e27a875f1e2900
Alexei <streiker.saik@gmail.com>
2025-07-27 01:17:12 +0300

    startapp users + settings + model User

--- config/settings.py
+++ config/settings.py
@@ -29,6 +29,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
+    "rest_framework",
+    "users",
+
 ]
 
 MIDDLEWARE = [
@@ -122,3 +126,5 @@ MEDIA_ROOT = BASE_DIR / "media"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "users.User"
---
+++ users/models.py
@@ -0,0 +1,36 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    """
+    Представление кастомного пользователя, расширяющее AbstractUser.
+    Поле авторизации с username изменено на email.
+    Так же username обязательное поле при авторизации
+    Атрибуты:
+        username(str): Уникальный логин
+        email(str): Уникальный email
+        phone_number(str): Номер телефона
+        city(str): Город
+        avatar(ImageField): Аватар (изображение)
+    """
+
+    username = models.CharField(max_length=150, verbose_name="Логин")
+    email = models.EmailField(unique=True, verbose_name="Email")
+    phone_number = models.CharField(max_length=15, blank=True, null=True, verbose_name="Номер телефона")
+    city = models.CharField(max_length=65, blank=True, null=True, verbose_name="Город")
+    avatar = models.ImageField(upload_to="avatars/", blank=True, null=True, verbose_name="Аватар")
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username"]
+
+    def __str__(self) -> str:
+        """
+        Строковое представление класса.
+        :return: Email
+        """
+        return f"{self.email}"
+
+    class Meta:
+        verbose_name = "пользователь"
+        verbose_name_plural = "пользователи"
